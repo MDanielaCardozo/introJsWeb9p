dce69029fc8aaa616c90d16228185edd74981fa3
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-06-08 19:58:16 -0300

    agregue conceptos iniciales de variables y tipos de datos

---
+++ js/01-variables.js
@@ -0,0 +1,73 @@
+//esto en un comentario de una linea
+
+/* comentario 
+multilinea */
+
+let precioCompra;
+
+let descuento;
+
+let descuentoAplicado;
+
+let nombre;
+
+nombre = "Pilar";
+
+console.log("Hola mundo");
+console.log(nombre);
+console.log("Contenido de la variable " + nombre);
+
+nombre = "Franco";
+
+console.log("Contenido de la variable " + nombre);
+
+const edad = 24;
+
+//edad = 25;
+
+//Tipo de variables
+
+let number = 3; //Variable mutable, respeta scope local y global.
+
+var name = "Juan"; //Variable mutable, no respeta scope local y global.
+
+const apellido = "Diaz"; //Variable inmutable, respeta scope(ambito).
+
+console.log(`Bienvenido ${apellido}`);
+console.info("Hola mundo");
+console.warn("Hola mundo");
+console.error("Hola mundo");
+alert("Bienvenido")
+
+/* const nombreUsuario = prompt("Ingresa tu nombre")
+
+console.log(nombreUsuario);
+
+document.writeln(`Bienvenido ${nombreUsuario}`)
+ */
+/* let num1 = Number(prompt("Ingrese un numero")); */
+//let num1 = parseInt(prompt("Ingrese un numero"))
+//let num1 = parseFloat(prompt("Ingrese un numero"))
+
+/* let num2 = Number(prompt("Ingrese otro numero"));
+
+let suma;
+
+suma = num1 + num2; */
+
+//document.writeln(`El valor de la suma de los numeros ingresados es : ${suma}`)
+
+console.log(2 == "2"); //igualdad debil
+
+console.log(2 === "2"); //igualdad estricta
+
+
+
+
+
+
+
+
+
+
+
